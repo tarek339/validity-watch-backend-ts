c9c39c63cfd8163932c6aa3fedfd8f037444b3a3
Tarek Jassine <tarekjassine@gmail.com>
2023-06-18 17:57:27 +0200

    verify email and password controllers

--- src/controller/companyController.ts
+++ src/controller/companyController.ts
@@ -175,8 +175,6 @@ const signIn = async (req: Request, res: Response, next: NextFunction) => {
         ceo: company.ceo,
         phoneNumber: company.phoneNumber,
         email: company.email,
-        password: company.password,
-        confirmPassword: company.confirmPassword,
         street: company.street,
         houseNumber: company.houseNumber,
         zipCode: company.zipCode,
@@ -211,7 +209,6 @@ const getProfile = async (req: Request, res: Response, next: NextFunction) => {
 const editProfile = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const company = await Company.findById(req.body.companyId);
-    console.log(company);
 
     company.firstName = req.body.firstName;
     company.lastName = req.body.lastName;
@@ -227,7 +224,163 @@ const editProfile = async (req: Request, res: Response, next: NextFunction) => {
     await company.save();
     res.json({
       message: "Profile data successfully changed",
-      company,
+      company: {
+        _id: company._id,
+        firstName: company.firstName,
+        lastName: company.lastName,
+        companyName: company.companyName,
+        ceo: company.ceo,
+        phoneNumber: company.phoneNumber,
+        email: company.email,
+        street: company.street,
+        houseNumber: company.houseNumber,
+        zipCode: company.zipCode,
+        city: company.city,
+        communityLicence: company.communityLicence,
+        emailVerified: company.emailVerified,
+      },
+    });
+  } catch (err) {
+    res.status(422).json({
+      message: mongooseErrorHandler(err as Error),
+    });
+  }
+};
+
+//Edit E-Mail
+const editEmail = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const company = await Company.findById(req.body.companyId);
+    const emailExist = await Company.findOne({
+      email: req.body.email,
+    });
+
+    if (emailExist) {
+      return res.status(422).json({
+        message: "E-mail allready exists!",
+      });
+    }
+    company.email = req.body.email;
+    company.emailVerified = false;
+
+    const token = crypto.randomBytes(32).toString("hex");
+    company.emailVerificationToken = token;
+
+    await company.save();
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "tarekjassine@gmail.com",
+        pass: "wonoytjxbqgxhjtm",
+      },
+    });
+    await transport.sendMail({
+      from: "tarekjassine@gmail.com",
+      to: company.email,
+      subject: "Verify your email",
+      html: `<p>Verify your email</p>
+            <a href="http://localhost:3000/verify-email?token=${token}">click here to verify your new e-mail</a>      
+      `,
+    });
+
+    res.json({
+      message: "E-mail changed, please verify it!",
+      company: {
+        _id: company._id,
+        firstName: company.firstName,
+        lastName: company.lastName,
+        companyName: company.companyName,
+        ceo: company.ceo,
+        phoneNumber: company.phoneNumber,
+        email: company.email,
+        street: company.street,
+        houseNumber: company.houseNumber,
+        zipCode: company.zipCode,
+        city: company.city,
+        communityLicence: company.communityLicence,
+        emailVerified: company.emailVerified,
+      },
+    });
+  } catch (err) {
+    res.status(422).json({
+      message: mongooseErrorHandler(err as Error),
+    });
+  }
+};
+
+// verify password for change password function
+const verifyPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const company = await Company.findById(req.body.companyId);
+
+    if (company.password != req.body.password) {
+      return res.status(422).json({
+        message: "password incorrect!",
+      });
+    }
+    res.json({ message: "password verified" });
+  } catch (err) {
+    res.status(422).json({
+      message: mongooseErrorHandler(err as Error),
+    });
+  }
+};
+
+// change password
+const changePassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const company = await Company.findById(req.body.companyId);
+    const oldPassword = await Company.findOne({ password: req.body.password });
+    if (oldPassword) {
+      return res.status(422).json({
+        message: "Changin to previous password is not allowed",
+      });
+    }
+    company.password = req.body.password;
+    company.confirmPassword = req.body.confirmPassword;
+
+    await company.save();
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "tarekjassine@gmail.com",
+        pass: "wonoytjxbqgxhjtm",
+      },
+    });
+    await transport.sendMail({
+      from: "tarekjassine@gmail.com",
+      to: company.email,
+      subject: "Change password",
+      html: `<p>Password successfully changed</p>`,
+    });
+
+    res.json({
+      message: "Password successfully changed",
+      company: {
+        _id: company._id,
+        firstName: company.firstName,
+        lastName: company.lastName,
+        companyName: company.companyName,
+        ceo: company.ceo,
+        phoneNumber: company.phoneNumber,
+        email: company.email,
+        street: company.street,
+        houseNumber: company.houseNumber,
+        zipCode: company.zipCode,
+        city: company.city,
+        communityLicence: company.communityLicence,
+        emailVerified: company.emailVerified,
+      },
     });
   } catch (err) {
     res.status(422).json({
@@ -242,4 +395,7 @@ module.exports = {
   signIn,
   getProfile,
   editProfile,
+  editEmail,
+  verifyPassword,
+  changePassword,
 };
