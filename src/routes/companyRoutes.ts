135bfcf4133d94c1533ff1bfb782e5d447b8fd49
Tarek Jassine <tarekjassine@gmail.com>
2023-07-02 13:14:21 +0200

    add

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -11,6 +11,7 @@ const {
   sendForgotPasswordEmail,
   forgotPasswordHandler,
   deleteAcc,
+  getCompanyProperties,
 } = require("../controller/company/companyController");
 const withSignIn = require("../middlewares/withSignIn");
 const companyFormValidation = require("../controller/company/companyFormValidation");
@@ -20,13 +21,17 @@ const router = express.Router();
 router.post("/sign-up", companyFormValidation, signUp);
 router.post("/sign-in", signIn);
 router.post("/verify-account", verifyAccount);
+router.post("/forgot-password-email", sendForgotPasswordEmail);
+router.post("/verify-password", withSignIn, verifyPassword);
+
 router.get("/profile", withSignIn, getProfile);
+router.get("/properties", withSignIn, getCompanyProperties);
+
 router.put("/edit-profile", withSignIn, editProfile);
 router.put("/edit-email", withSignIn, editEmail);
-router.post("/verify-password", withSignIn, verifyPassword);
 router.put("/change-password", withSignIn, changePassword);
-router.post("/forgot-password-email", sendForgotPasswordEmail);
 router.put("/forgot-password-handler", forgotPasswordHandler);
+
 router.delete("/delete-acc", withSignIn, deleteAcc);
 
 module.exports = router;
